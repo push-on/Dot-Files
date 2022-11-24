4234b3378a7bedb4d3a370902a56c9a71bfd8661
Pushon <50992812+push-on@users.noreply.github.com>
2022-11-24 20:40:00 +0600

    Create site_filters.js

---
+++ site_filters.js
@@ -0,0 +1,16 @@
+! 2022-11-17 https://www.youtube.com
+www.youtube.com##ytd-guide-section-renderer.ytd-guide-renderer.style-scope:nth-of-type(2)
+
+! 2022-11-17 https://www.facebook.com
+www.facebook.com##.x12nzpgo.x1lku1pv.x5ib6vp.x18d9i69.xc73u3c.xexx8yu.xt55aet.x1c4vz4f.xb756pt.x1q0g3np.xozqiw3.x6s0dn4.x1qughib.xeuugli.xs83m0k.x1r8uery.x78zum5.x1ja2u2z.x1n2onr6.x9f619
+
+! 2022-11-17 https://www.reddit.com
+www.reddit.com##.HNozj_dKjQZ59ZsfEegz8._10BQ7pjWbeYP63SAPNS8Ts._2iuoyPiKHN3kfOoeIQalDT.q_unSaY23rpdd3lDvGZ-._1_-T-UyaTFWSKiU5qkmH4p
+www.reddit.com##div._2vEf-C2keJaBMY9qk_BxVn > ._2QeqBqfT5UbHBoViZUt-wX._1G4yU68P50vRZ4USXfaceV
+www.reddit.com##._10wb7d3rGvj56Gcs4IQWL5._1FUNcfOeszr8eruqLxCMcR > ._2QeqBqfT5UbHBoViZUt-wX._1G4yU68P50vRZ4USXfaceV
+
+! 2022-11-24 https://www.geeksforgeeks.org
+www.geeksforgeeks.org##.practiceBannerFromPlugin
+
+! 2022-11-24 https://www.scrum.org
+www.scrum.org##.last-p-mb-0.text-align-center.fs-9.generic-page-banner
